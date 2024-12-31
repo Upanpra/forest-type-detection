143c85292133245dd594f25ba5f378d77420b15a
Upanpra <partiksai9@gmail.com>
2024-12-30 22:50:11 -0800

    Update predict_HWSW.py

--- predict_HWSW.py
+++ predict_HWSW.py
@@ -3,7 +3,7 @@ from src.convnext import convnext_single_block_hyperspectral, convnext_single_bl
     convnext_minimal_hyperspectral, convnext_minimal_hyperspectral_dropout, \
     convnext_minimal_hyperspectral_final_layer_dropout
 from src.minimal_cnn import min_cnn_dropout, min_cnn
-from src.model import DroughtCNN
+from src.model import ForestCNN
 from src.predict.dataset import get_predict_loader
 from src.predict.predictor import predictCNN
 from src.functions import *
@@ -41,24 +41,21 @@ NUM_EPOCHS = 100           # change to 200 # The number of times we loop over th
 BATCH_SIZE = 16           # Change to 16 or 32 The size of input data took for one iteration of an epoch
 LEARNING_RATE = 1e-3          # The speed of convergence
 
-#OUTPUT_FOLDER = "/mmfs1/gscratch/stf/upanpra/model_checkpoints/" # For Hyak
-
 # Prediction for inference area
 tiff_input_folder = ['data/unlabeled/vis',
-                'data/unlabeled/chm'] #directory of your unlabeled crops such as vis, chm, dtm
+                'data/unlabeled/chm'] #directory of your unlabeled crops such as vis, chm, dtm....
 
 tiff_output_folder = "data/prediction/hardwood-softwood-nonforest/" # directory of predicted files
 
 
 # Define model name
-model_name = "DroughtCNN"
+model_name = "ForestCNN"
 model_path = "data/models/checkpoint/ "#path to saved model checkpoint
 
 # use transforms_aug function from data loader to do augmentation
 transform_train, transform_test = transforms_aug(INPUT_SIZE, mean, std)
 
-all_models = {#"resnet18": partial(resnet18, input_channels=44),
-            "DroughtCNN": partial(DroughtCNN, input_size=[INPUT_CHANNELS] + INPUT_SIZE, num_classes=NUM_CLASSES),
+all_models = {"ForestCNN": partial(ForestCNN, input_size=[INPUT_CHANNELS] + INPUT_SIZE, num_classes=NUM_CLASSES),
             "min_cnn": partial(min_cnn, num_classes=NUM_CLASSES),
             "min_cnn_dropout": partial(min_cnn_dropout, num_classes=NUM_CLASSES),
             "conv_next_single_block_model": partial(convnext_single_block_hyperspectral, num_classes=NUM_CLASSES),
